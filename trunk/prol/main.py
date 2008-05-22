900d9836219e27aaa84e64a3290b8e4f93016463
pureza <pureza@079ecbc8-1c4d-0410-a979-7344a2d2a553>
2008-05-22 18:13:15 +0000

    Ok, algorithm is complete

--- trunk/prol/main.py
+++ trunk/prol/main.py
@@ -1,3 +1,27 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+prol-evolution GRN, a model for regulatory gene networks
+
+Copyright (C) 2008 Luis Pureza, Oseias Santos, Pedro Matrins and
+Ricardo Pereira.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 from genome import *
 import gene
 import rna
@@ -6,46 +30,17 @@ import graph
 import random
 import pydot
 
-# Exemplo do paper
-#genome = Genome("1111121203221103300301011013232200121230320022321230302031111")
+avg = lambda l: sum(l) / len(l)
+miRNA_binding_site_size = 6
+protein_gene_binding_site_size = 6
+protein_gene_binding_threshold = 0.34
 
-if __name__ == '__main__':
-    miRNA_binding_site_size = 6
-    protein_gene_binding_site_size = 6
-    protein_gene_binding_threshold = 0.3
 
-    genes = generate_random_genome(100000).get_genes()
-    mRNAs = set()
-    ncRNAs = []
-    for gene in genes:       
-        (mRNA, gene_ncRNAs) = gene.splice()
-	mRNAs.add(mRNA)
-	ncRNAs += gene_ncRNAs
-
-    proteins = []
-    to_remove = set()
-    for mRNA in mRNAs:
-        protein = mRNA.translate()
-	if protein != None:
-	    proteins.append(protein)
-	else:
-	    # if the mRNA does not contain the start codon, add it to the
-	    # list of non-coding RNA
-	    ncRNAs.append(NonCodingRNA(mRNA.parent, mRNA.sequence))
-            to_remove.add(mRNA)
-
-    mRNAs -= to_remove
-
-    miRNAs = []
-    for ncRNA in ncRNAs:
-	miRNAs += ncRNA.create_miRNAs(miRNA_binding_site_size)
-
-    print "Number of genes: %d" % len(genes)
-    print "Number of mRNAs: %d" % len(mRNAs)
-    print "Number of proteins: %d" % len(proteins)
-    print "Number of ncRNA: %d" % len(ncRNAs)
-    print "Number of miRNA: %d" % len(miRNAs)
-
+def create_graph(genes, mRNAs, ncRNAs, proteins, miRNAs):
+    """
+    Creates the graph containing the gene regulatory network
+    """
+    
     grn = graph.graph()
     grn.add_nodes(genes)
     grn.add_nodes(mRNAs)
@@ -75,31 +70,76 @@ if __name__ == '__main__':
 
     # Create connections between proteins and genes
     for protein in proteins:
-	for gene in genes:
-            if protein.binds_to_gene(lambda l: sum(l) / len(l), gene, protein_gene_binding_site_size, protein_gene_binding_threshold):
+        for gene in genes:
+            if protein.binds_to_gene(avg, gene, \
+                                     protein_gene_binding_site_size, \
+                                     protein_gene_binding_threshold):
                 grn.add_edge(protein, gene, wt=random.choice([-1, 1]))
 
-    # Todo
-    # Create connections between miRNA and mRNA
+    # Create connections between miRNAs and mRNAs
+    for miRNA in miRNAs:
+        for mRNA in mRNAs:
+            if miRNA.binds_to_mRNA(mRNA):
+                grn.add_edge(miRNA, mRNA, -1)
 
-    #print len(grn.weights)
+    return grn
+
+
+if __name__ == '__main__':
+    # Exemplo do paper
+    #genes = Genome("1111121203221103300301011013232200121230320022321230302031111").get_genes()
+
+    # Generate random genome
+    genes = generate_random_genome(100000).get_genes()
+    mRNAs = set()
+    ncRNAs = []
+
+    # Splice the genes
+    for gene in genes:       
+        (mRNA, gene_ncRNAs) = gene.splice()
+	mRNAs.add(mRNA)
+	ncRNAs += gene_ncRNAs
+
+    # Translate the mRNAs into proteins
+    proteins = []
+    to_remove = set()
+    for mRNA in mRNAs:
+        protein = mRNA.translate()
+	if protein != None:
+	    proteins.append(protein)
+	else:
+	    # if the mRNA does not contain the start codon, add it to the
+	    # list of non-coding RNA
+	    ncRNAs.append(NonCodingRNA(mRNA.parent, mRNA.sequence))
+            to_remove.add(mRNA)
+
+    # Remove mRNAs without the start codon from the list of mRNAs
+    mRNAs -= to_remove
+
+    # Create miRNAs
+    miRNAs = []
+    for ncRNA in ncRNAs:
+	miRNAs += ncRNA.create_miRNAs(miRNA_binding_site_size)
+
+    # Create the graph
+    grn = create_graph(genes, mRNAs, ncRNAs, proteins, miRNAs)
+
     edges = []
-    
-    for node in grn.get_nodes():
+    for i in range(len(grn.get_nodes())):
+        node = grn.get_nodes()[i]
         for edge in grn.nodes[node]:
             edges += (str(node), str(edge))
+
+#    for node in grn.get_nodes():
+#        for edge in grn.nodes[node]:
+#            edges += (str(node), str(edge))
     
     g = pydot.graph_from_edges(edges)
     g.write('banana.dot');
-    
-    
-    #g.write_jpeg('graph_from_edges_dot.jpg', prog='dot')  
-#                blah[protein] = gene
-
-#	        print "%s binds to %s" % (str(protein), str(gene))
 
 
-
-    # TODO
-    # Find interactions between miRNAs and genes
-    # Create the graph
+    print "Number of genes: %d" % len(genes)
+    print "Number of mRNAs: %d" % len(mRNAs)
+    print "Number of proteins: %d" % len(proteins)
+    print "Number of ncRNA: %d" % len(ncRNAs)
+    print "Number of miRNA: %d" % len(miRNAs)
--- trunk/prol/protein.py
+++ trunk/prol/protein.py
@@ -1,3 +1,24 @@
+"""
+prol-evolution GRN, a model for regulatory gene networks
+
+Copyright (C) 2008 Luis Pureza, Oseias Santos, Pedro Matrins and
+Ricardo Pereira.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 from defs import *
 from grn_element import *
 
@@ -8,46 +29,49 @@ class Protein(GRNElement):
 	GRNElement.__init__(self, parent)
         self.sequence = sequence
 
-    def dna_aminoacids_binding(self, func, dna, aminoacids):
+    def dna_aminoacids_bindings(self, dna, aminoacids):
         """
-        Finds the binding probability between a sequence of DNA and a sequence of
-        aminoacids.
+        Returns the binding probabilities between a sequence of DNA
+        and a sequence of aminoacids.
         """
-        return func([binding_table[(a, int(b))] / 100.0 for (b, a) in zip(dna, aminoacids)])
+        return [binding_table[(a, int(b))] / 100.0
+                for (b, a) in zip(dna, aminoacids)]
 
     def binds_to_gene (self, func, gene, binding_size, binding_threshold):
         """
         Checks if this proteins binds to some gene. 
         
         Given:
-        - prot_segment is some protein segment with binding_size size
-        - gene_segment is some protein dna sequence with binding_size size, from
+        - prot_seg is some protein segment with binding_size size
+        - gene_seg is some protein dna sequence with binding_size size, from
           the gene's regulatory region
         
-        The protein will bind if dna_protein_binding(...) is bigger than the given
-        threshold
+        The protein will bind if dna_protein_binding(...) is bigger than the
+        given threshold
         
         . Parameters:
         - func: method of selection of the binding probability from all the
-        probablities between bases and aminoacids (max, min, avg, ...)
+          probablities between bases and aminoacids (max, min, avg, ...)
         - gene: The gene.
-        - binding_size: Minimum size of the binding site between the gene and the
-        protein.
+        - binding_size: Minimum size of the binding site between the gene and
+          the protein.
         - binding_threshold: Minimum value for the selected binding probability
-        for binding to occur.
+          for binding to occur.
         """
         if len(self.sequence) >= binding_size:
             if len(gene.regulatory_region) >= binding_size:
-                # for each possible protein segment with size = binding site size
+                # foreach possible protein segment with size = binding site size
                 for pi in range(len(self.sequence) - binding_size + 1):
-                    prot_segment = self.sequence[pi:pi + binding_size]
+                    prot_seg = self.sequence[pi:pi + binding_size]
                     
-                    # for each possible dna sequence with size = binding site size
-                    for gi in range(len(gene.regulatory_region) - binding_size + 1):
-                        gene_segment = gene.regulatory_region[gi:gi + binding_size]
+                    # foreach possible dna seq. with size = binding site size
+                    for gi in range(len(gene.regulatory_region) - \
+                                    binding_size + 1):
+                        gene_seg = gene.regulatory_region[gi:gi + binding_size]
                         
                         # Check if they bind
-                        if self.dna_aminoacids_binding(func, gene_segment, prot_segment) >= binding_threshold:
+                        if func(self.dna_aminoacids_bindings(gene_seg, prot_seg)) \
+                               >= binding_threshold:
                             return True
                         
         return False        
