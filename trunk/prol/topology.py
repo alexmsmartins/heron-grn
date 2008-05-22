988d7af9a948a15af88a08ac24f32347578d05f1
alexmsmartins <alexmsmartins@079ecbc8-1c4d-0410-a979-7344a2d2a553>
2008-05-22 16:41:44 +0000

    adicao do ficheiro que contera as funções de queficientes associados a grafos

---
+++ trunk/prol/topology.py
@@ -0,0 +1,12 @@
+from pydot import *
+
+
+def small_worlds_from_file(dot_file):
+    """recebe um nome de ficeiro como input e 
+    retorna o """
+        small_worlds( graph_from_dot_file(path))
+    
+def small_worlds(graph):
+    """recebe um objecto Dot como input e 
+    retorna o """
+    
