6eecbd8d029e61bd9ae70def6bb6834064250ff0
oseias.djrock <oseias.djrock@079ecbc8-1c4d-0410-a979-7344a2d2a553>
2008-05-24 20:39:21 +0000

    Topology com a funcao de determinacao de propriedades de scale free determinadas

--- prol/topology.py
+++ prol/topology.py
@@ -1,16 +1,38 @@
 # coding=UTF-8
+"""
+prol-evolution GRN, a model for regulatory gene networks
+
+Copyright (C) 2008 Luis Pureza, Oseias Santos, Pedro Martins and
+Ricardo Pereira.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import pydot
 import networkx as NX
+import math
+from math import sqrt
+from numpy import *
 
 def small_worlds_from_file(path):
     """
     recebe um nome de ficeiro como input e 
-    retorna se esta rede é small world ou não de
-    acordo com os critérios definidos.
-    Um grafo é considerado small-world se Ci for maior
-    do que um random graph com o mesmo set de vértices
-    e se tem uma média das distâncias mais curtas baixa.
+    retorna se esta rede Ã© small world ou nÃ£o de
+    acordo com os critÃ©rios definidos.
+    Um grafo Ã© considerado small-world se Ci for maior
+    do que um random graph com o mesmo set de vÃ©rtices
+    e se tem uma mÃ©dia das distÃ¢ncias mais curtas baixa.
     """
     return small_worlds(dot_to_NXGraph(pydot.graph_from_dot_file(path)))
     
@@ -31,20 +53,20 @@ def small_worlds(graph):
 def calc_random_graph_clust_coef(graph):
     """
     Calcula o coeficiente de clustering de um random graph com
-    o mesmo número de nós que o grafo fornecido
+    o mesmo nÃºmero de nÃ³s que o grafo fornecido
     """
     return calc_avg_edge_count(graph) / graph.size()
 
 def calc_random_graph_avg_smallest_path(graph):
     """
-    Calcula a média de distências mais curtas de um random graph com
-    o mesmo número de nós que o grafo fornecido
+    Calcula a mÃ©dia de distÃªncias mais curtas de um random graph com
+    o mesmo nÃºmero de nÃ³s que o grafo fornecido
     """
     return log(graph.size()) / log(calc_avg_edge_count(graph))
 
 def calc_avg_graph_shortest_path(graph):
     """
-    Calcula a média das distâncias mais curtas entre todos os nós
+    Calcula a mÃ©dia das distÃ¢ncias mais curtas entre todos os nÃ³s
     """
     listshortestpaths = NX.path.all_pairs_shortest_path_length(graph)
     avg = 0
@@ -70,7 +92,7 @@ def dot_to_NXGraph(dotgraph):
 
 def calc_avg_edge_count(graph):
     """
-    Calcula o número médio de ligações por nó
+    Calcula o nÃºmero mÃ©dio de ligaÃ§Ãµes por nÃ³
     """
     avg = 0;
     
@@ -81,23 +103,77 @@ def calc_avg_edge_count(graph):
 
 def scale_free(graph):
     """
-    Recebe o grafo e verifica se é small world
+    Recebe o grafo e verifica se Ã© small world
     em caso positivo verifica se existem clusters no grafo
     """
-    if not small_worlds(graph):
-        return False
     
-    numHigerClust = 0
-    media = NX.average_clustering(graph) * 1.1
+    #Array de graus
+    array = graph.degree()
+
+    #lista de pontos com o mesmo grau
+    list = {}
+    for i in array:
+        try:
+            list[i] = list[i] + 1
+        except:
+            list[i] = 1
+
+    list_log_x = []
+    list_log_y = []
+    #list_log = {}
+    #avg_listlog_x = 0
+    #avg_listlog_y = 0
+    for k, v in list.iteritems():
+        list_log_x = list_log_x + [math.log(k)]
+        list_log_x = list_log_y + [math.log(v)]
+        #avg_listlog_x += math.log(k)
+        #avg_listlog_y += math.log(v)
+
+
+
+
+
+
+
+
+
+
+    #avg_listlog_x = avg_listlog_x/len(list_log)
+    #avg_listlog_y = avg_listlog_y/len(list_log)
+
+
+
+
+
+    """
+    numerator = 0
+    denominator = 0
+    for k, v in list_log.iteritems():
+        numerator = k * v
+        denominator = k*k
     
-    for x in NX.clustering(graph):
-        if x > media :
-            numHigerClust += 1
+    numerator = numerator - len(list_log)*avg_listlog_x*avg_listlog_y
+    denominator = denominator - len(list_log)*avg_listlog_x*avg_listlog_x
     
-    return (numHigerClust / graph.size()) < 0.1
-                
+    
+    b = numerator/denominator
+    a = avg_listlog_y - b*avg_listlog_x
+    """
+    coefficients = polyfit(x,y,1)
+    
+    print quadratic_error(coefficients[0], coefficients[1], list_log)
+    
+    
+def quadratic_error(a, b, list):
+    d = 0;
+    for x, y in list.iteritems():
+        d += pow(y - a*x+b, 2)
+    return sqrt(d/len(list))
+
+      
 def scale_free_from_file(path):
     """
-    recebe um ficheiro para verificar se o grafo é scale free
+    recebe um ficheiro para verificar se o grafo Ã© scale free
     """
+
     return scale_free(dot_to_NXGraph(pydot.graph_from_dot_file(path)))
